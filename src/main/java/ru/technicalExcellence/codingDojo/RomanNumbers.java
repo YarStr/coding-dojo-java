42e5c97ad09c1646edfb2184dc2a9660cd79c05a
Nikon <102325439+MrNikaMilon@users.noreply.github.com>
2025-04-26 14:35:18 +0300

    add romans II

--- src/main/java/ru/technicalExcellence/codingDojo/RomanNumbers.java
+++ src/main/java/ru/technicalExcellence/codingDojo/RomanNumbers.java
@@ -6,7 +6,12 @@ public class RomanNumbers {
     private static final Map<Integer, String> romanArabicaDictonary = Map.of(
             1, "I",
             5, "V",
-            10, "X"
+            10, "X",
+            50, "L",
+            100, "C",
+            500, "D",
+            1000, "M"
+
     );
 
     public String get(int number) {
--- src/test/java/ru/technicalExcellence/codingDojo/RomanNumbersShould.java
+++ src/test/java/ru/technicalExcellence/codingDojo/RomanNumbersShould.java
@@ -3,6 +3,7 @@ package ru.technicalExcellence.codingDojo;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -26,12 +27,18 @@ public class RomanNumbersShould {
             "1, I",
             "5, V",
             "10, X",
+            "50, L",
             "100, C",
             "500, D",
             "1000, M"
     })
-    void return_I_when_1_is_given(Integer input, String expected) {
+    void return_roman_when_arabic_numbers_is_given(Integer input, String expected) {
         assertEquals(expected, sut.get(input));
 
+    }
+
+    @Test
+    void return_II_then_2_is_given(){
+        assertEquals("II", sut.get(2));
     }
 }
